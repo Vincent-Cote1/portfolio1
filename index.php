c9c039dac54faf4fcab046449cfb17a7b017399c
Vincent-Cote1 <2378268@cstj.qc.ca>
2026-08-16 14:43:25 -0400

    ajout sections du portfolio

--- index.php
+++ index.php
@@ -6,8 +6,23 @@
     <title>Portfolio</title>
 </head>
 <body>
-    <p>Hello, World!</p>
-    <p>allo maman</p>
-    <p>allo papa</p>
+    <h1>Portfolio</h1>
+    <p>Vincent Côté, étudiant en technique d'intégration multimédia au cégep de Saint-Jérôme</p>
+    
+    <h2>Projets</h2>
+    <ul>
+        <li>
+            <p>Section Jeux Vidéo</p>
+        </li>
+        <li>
+            <p>Section Web</p>
+        </li>
+        <li>
+            <p>Section Design</p>
+        </li>
+        <li>
+            <p>Section Vidéo</p>
+        </li>
+    </ul>
 </body>
 </html>
