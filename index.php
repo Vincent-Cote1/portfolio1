793e082be645ad0258c909dfae24a793467e09fb
Vincent-Cote1 <2378268@cstj.qc.ca>
2026-08-16 14:50:59 -0400

    ajout include menu-nav

--- index.php
+++ index.php
@@ -7,23 +7,6 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Portfolio</h1>
-    <p>Vincent Côté, étudiant en technique d'intégration multimédia au cégep de Saint-Jérôme</p>
-    
-    <h2>Projets</h2>
-    <ul>
-        <li>
-            <a href="jeu.php">Jeux Vidéo</a>
-        </li>
-        <li>
-            <a href="web.php">Web</a>
-        </li>
-        <li>
-            <a href="design.php">Design</a>
-        </li>
-        <li>
-            <a href="video.php">Vidéo</a>
-        </li>
-    </ul>
+    <?php include 'menu-nav.php'; ?>
 </body>
 </html>
